14c6e2cb2ae99330873f4683c27723cd8e559dd7
angelina-tsuboi <angelina.t1832@gmail.com>
2021-01-29 00:36:09 +0900

    feat: show github data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'testing-github-actions';
+  users;
+
+  constructor(private useService: UserService){
+
+  }
+
+  ngOnInit(){
+     this.users = this.useService.getUsers();
+  }
 }
